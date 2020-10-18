cee0ff1f0218fc9f70e3c0beeffd048d006092f6
vishal <vvishwakarma@gmail.com>
2020-10-18 13:50:54 +0530

    array practise

--- test.java
+++ test.java
@@ -1,5 +1,23 @@
 class test{
     public static void main(String[] args) {
-        System.out.println("Hello welcome to VS CODE");
+        System.out.println("Hello welcome to finding maximum of an given array");
+        int[] A = {5,7,12,34,67,87,1};
+        int max=A[0];
+        for (int i=0;i<A.length;i++){
+            if(A[i]>max){
+                max=A[i];
+            }
+        }
+        int min=A[0];
+        for (int i=0;i<A.length;i++){
+            if(A[i]<min){
+                min=A[i];
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
+
     }
 }
+
+    
